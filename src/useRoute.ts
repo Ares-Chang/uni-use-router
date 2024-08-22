ef97d452b5ba87bcb391dec170e1dab6003692a4
Chang <areschang114@qq.com>
2024-08-22 13:26:27 +0800

    perf: 优化 useRoute 获取逻辑，防止拦截器中获取失败

--- src/useRoute.ts
+++ src/useRoute.ts
@@ -30,11 +30,7 @@ export function useRoute() {
     matched: [],
   })
 
-  /**
-   * 不包含在 setTimeout 中 options 和 $page 中的值在小程序端可能取不出来
-   * @TODO: 逻辑肯定有问题，暂时没找到好的方法替代
-   */
-  setTimeout(() => {
+  function setObj() {
     const pages = getCurrentPages()
     const current = pages[pages.length - 1] as _PageInstance
 
@@ -45,7 +41,15 @@ export function useRoute() {
     obj.query = current.options || options || {}
     obj.current = current
     obj.matched = pages
-  }, 0)
+  }
+  // 首次调用，方便 interceptor 中使用，不影响页面中使用
+  setObj()
+
+  /**
+   * 不包含在 setTimeout 中 options 和 $page 中的值在小程序端可能取不出来
+   * @TODO: 逻辑肯定有问题，暂时没找到好的方法替代
+   */
+  setTimeout(setObj, 0)
 
   return obj
 }
